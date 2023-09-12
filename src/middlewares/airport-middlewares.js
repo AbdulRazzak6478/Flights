a008e182ba10926fa90911908afe6bd16693bcca
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-09-13 01:44:55 +0530

    airport middleware request errors handled

--- src/middlewares/airport-middlewares.js
+++ src/middlewares/airport-middlewares.js
@@ -4,22 +4,23 @@ const { ErrorResponse} = require('../utils/common');
 const AppError = require("../utils/errors/app-error");
 
 function validateCreateRequest(req, res, next) {
+  if(req.body.name && req.body.code && req.body.cityId)
+  {
+    next();
+  }
+  ErrorResponse.message =  'Something went wrong while creating airport';
+  ErrorResponse.error = [];
   if (!req.body.name) {
-    ErrorResponse.message =  'Something went wrong while creating airport';
-    ErrorResponse.error = new AppError(['model number is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST)
-    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    ErrorResponse.error.push(new AppError(['Airport name is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST))
   }
   if (!req.body.code) {
-    ErrorResponse.message =  'Something went wrong while creating airport';
-    ErrorResponse.error = new AppError(['Airport code is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST)
-    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    ErrorResponse.error.push(new AppError(['Airport code is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST))
   }
   if (!req.body.cityId) {
-    ErrorResponse.message =  'Something went wrong while creating airport';
-    ErrorResponse.error = new AppError(['Airport cityId is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST)
-    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    ErrorResponse.error.push(new AppError(['Airport cityId is not found in onComing request in correct format'],StatusCodes.BAD_REQUEST))
   }
-  next();
+  return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  // next();
 }
 
 module.exports = {
